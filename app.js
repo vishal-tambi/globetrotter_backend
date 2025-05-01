b928542a50873016aa217ba3000ea6e8bfcb2774
Vishal Tambi <138616107+vishal-tambi@users.noreply.github.com>
2025-05-01 09:49:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:5173'],   
+  origin: ['https://globetrotter-frontend-omega.vercel.app'],   
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
